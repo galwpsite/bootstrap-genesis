00c5d0fd597db484b6ebb42cadc8ee7b0b7db47d
galwp <gal@wpsite.co.il>
2016-01-05 17:48:16 +0200

    Added LiveLoad
    in functions.php i've made sure that the browers will autorefresh.

--- functions.php
+++ functions.php
@@ -14,3 +14,11 @@ add_action( 'genesis_setup', 'bsg_load_lib_files', 15 );
 function bsg_load_lib_files() {
     foreach ( glob( dirname( __FILE__ ) . '/lib/*.php' ) as $file ) { include $file; }
 }
+
+if (in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'))) {
+  add_action("wp_enqueue_scripts","wpc_add_livereload");
+}
+function wpc_add_livereload(){
+  wp_register_script('livereload', 'http://localhost:35729/livereload.js?snipver=1', null, false, true);
+  wp_enqueue_script('livereload');
+}
